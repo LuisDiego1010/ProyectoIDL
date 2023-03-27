f81ac6ab6022417c35cccbac6bc55d0b0d6b6ee2
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2023-03-26 21:26:51 -0600

    Ventana principal y secundarias con interfaz

--- mainWindow.py
+++ mainWindow.py
@@ -1,45 +1,46 @@
+from tkinter import *
 import tkinter as tk
+from PIL import ImageTk, Image
 
 #******************Ventana Principal**********************
 mainwindow = tk.Tk()
 mainwindow.title('Menu principal: Codigo Hamming')
-canvas_principal = tk.Canvas(mainwindow, width=250, height=100)
+canvas_principal = tk.Canvas(mainwindow, width=850, height=500)
+file=PhotoImage(file="resources//main.png",master=mainwindow)
+canvas_principal.create_image(0,0,anchor=NW,image=file)
 canvas_principal.pack()
-#******************Ventana Principal**********************
 
 #******************Creacion de ventanas(Inicio)************************
 def openNewWindow_Hamming_Code():
     newWindow = tk.Toplevel(mainwindow)
     newWindow.title('Codigo Hamming')
-    #newWindow.geometry("960x540")    
-    canvas_principal1 = tk.Canvas(newWindow, width=500, height=450)
+    canvas_principal1 = tk.Canvas(newWindow, width=850, height=500)
+    bg1=PhotoImage(file="resources//Hamming Code.png",master=newWindow)
+    label2 = Label( canvas_principal1, image = bg1)
+    label2.image = bg1
+    label2.place(x = 0, y = 0)
     canvas_principal1.pack()
-    texto1 = tk.Label(newWindow, text= "This is a new window")
-    texto1.pack()
 
 def openNewWindow_Conversion():
     newWindow2 = tk.Toplevel(mainwindow)
     newWindow2.title('Conversion')
-    #newWindow2.geometry("960x540")
-    canvas_principal2 = tk.Canvas(newWindow2, width=500, height=450)
+    canvas_principal2 = tk.Canvas(newWindow2, width=850, height=500)
+    bg2 = PhotoImage(file = "resources//Conversion.png")
+    label2 = Label( canvas_principal2, image = bg2)
+    label2.image = bg2
+    label2.place(x = 0, y = 0)
     canvas_principal2.pack()
-    texto2 = tk.Label(newWindow2, text= "This is a new window")
-    texto2.pack()
-
 #******************Creacion de ventanas(FIN)************************
 
-label = tk.Label(mainwindow, text = "This is the main window")
-label.pack(pady = 10)
-
 btn_create_window_Hamming = tk.Button(mainwindow,
-             text ="Conversiones",
+             text ="Conversiones/NZRI",
              command = openNewWindow_Conversion)
-btn_create_window_Hamming.place(x=85, y=10)
+btn_create_window_Hamming.place(x=480, y=400)
 
 btn2_create_window_Conversion = tk.Button(mainwindow,
              text ="Codigo Hamming",
              command = openNewWindow_Hamming_Code)
-btn2_create_window_Conversion.place(x=75, y=50)
+btn2_create_window_Conversion.place(x=620, y=400)
 #top = Toplevel()
 
 
