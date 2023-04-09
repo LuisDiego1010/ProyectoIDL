6240e1d462176fe298b987fe54e703f073a40da5
Josue Santamaria <48070852+Santamix728@users.noreply.github.com>
2023-04-08 19:29:34 -0600

    Hamming
    Hamming code generator

---
+++ Hammin_code.py
@@ -0,0 +1,134 @@
+def calculate_redundant_bits(var):
+
+    if var == 1:
+        return 1
+    if var == 2 or var == 3:
+        return 2
+    for index in range(var):
+        if (2**index >= var + index + 1):
+            return index
+
+def get_hamming_string(parity_list, r):
+    res = ""
+    pos = 1
+    r_counter = 0
+    for i in parity_list[0]:
+        if is_power_of_two(pos):
+            while is_power_of_two(pos) and r_counter < r:
+                for j in parity_list[r_counter + 1]:
+                    if j.isnumeric():
+                        res = res + j
+                        break
+                pos += 1
+                r_counter += 1
+        res = res + i
+        pos += 1
+    return res
+
+def count_parity(pos, block_size, array):
+    counter = 0
+    bit_array = []
+    in_block_counter = 0
+    for i in range(0, pos):
+        bit_array.append("")
+    while pos < len(array):
+        if in_block_counter < block_size:
+            if array[pos] == '1':
+                counter += 1
+            bit_array.append(array[pos])
+            pos += 1
+            in_block_counter += 1
+        else:
+            for i in range(0, block_size):
+                bit_array.append("")
+            pos += block_size
+            in_block_counter = 0
+    return (counter, bit_array)
+
+def insert_parity_bits(data):
+    res = ""
+    pos = 1
+    for i in data:
+        if is_power_of_two(pos):
+            while is_power_of_two(pos):
+                res = res + '0'
+                pos += 1
+        res = res + i
+        pos += 1
+    return res
+
+def is_power_of_two(num):
+    while num != 1:
+        if num < 1:
+            return False
+        else:
+            num = num / 2
+    return True
+
+def get_hamming_array(data, r, parity_type):
+    array = insert_parity_bits(data)
+    res = []
+    res.append(data)
+    if parity_type:
+        for i in range(0, r):
+            parity_data = count_parity((2**i) - 1, 2**i, array)
+            if parity_data[0] % 2 == 1:
+                parity_data[1][0] = '1'
+            res.append(parity_data[1])
+    else:
+        for i in range(0, r):
+            parity_data = count_parity((2**i) - 1, 2**i, array)
+            if parity_data[0] % 2 == 0:
+                parity_data[1][0] = '1'
+            res.append(parity_data[1])
+    res.append(get_hamming_string(res, r))
+    return res
+
+def calculate_parity_bits_amount(data):
+    size_of_data = len(data)
+    for i in range(0, size_of_data):
+        if 2**i >= size_of_data:
+            return i
+
+def get_hamming_check(data,parity_type):
+    res = [data]
+    r = calculate_parity_bits_amount(data)
+    temp_list = []
+    pos_error = ""
+    if parity_type:
+        for i in range(0,r):
+            parity_data = count_parity((2**i)-1,2**i, data)
+            if parity_data[0]%2 == 0:
+                temp_list.append('0')
+                temp_list.append("Correct")
+                pos_error = pos_error + "0"
+            else:
+                temp_list.append('1')
+                temp_list.append("Error")
+                pos_error = pos_error + "1"
+            temp_list.append(parity_data[1])
+            res.append(temp_list)
+            temp_list = []
+    else:
+        for i in range(0,r):
+            parity_data = count_parity((2**i)-1,2**i, data)
+            if parity_data[0]%2 == 1:
+                temp_list.append('0')
+                temp_list.append("Correct")
+                pos_error = pos_error + "0"
+            else:
+                temp_list.append('1')
+                temp_list.append("Error")
+                pos_error = pos_error + "1"
+            temp_list.append(parity_data[1])
+            res.append(temp_list)
+            temp_list = []
+    pos_error = pos_error[::-1]        
+    return (res,int(pos_error,2))
+
+def hamming_encode(data,parity_type):
+    return get_hamming_array(data,calculate_redundant_bits(len(data)),parity_type)
+
+def check_hamming_encode(data,parity_type):
+    return (get_hamming_check(data,parity_type))
+
--- mainWindow.py
+++ mainWindow.py
@@ -2,8 +2,16 @@ from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
 import turtle
+import Hammin_code
 
-
+#Color & fonts
+bg_color='#296ABC'
+text_color = '#CDE6F5'
+btn_color = '#ACF39D'
+t_font ='Times New Roman'
+t_font_size = 18
+subt_font_size = 16
+small_text_size = 12
 
 #******************Ventana Principal**********************
 mainwindow = tk.Tk()
@@ -17,13 +25,295 @@ canvas_principal.pack()
 def openNewWindow_Hamming_Code():
     newWindow = tk.Toplevel(mainwindow)
     newWindow.title('Codigo Hamming')
-    canvas_principal1 = tk.Canvas(newWindow, width=850, height=500)
+    canvas_principal1 = tk.Canvas(newWindow, width=895, height=500, background='#000000')
     bg1=PhotoImage(file="resources//Hamming Code.png",master=newWindow)
-    label2 = Label( canvas_principal1, image = bg1)
-    label2.image = bg1
-    label2.place(x = 0, y = 0)
+    label3 = Label( canvas_principal1,image=bg1,background='#000000')
+    label3.image = bg1
+    label3.place(x = 0, y = 0)
     print(binario_ext[2:]) #Valor binario extraído
-    bin_label = tk.Label( canvas_principal1, text= binario_ext[2:]).place(x = 30, y = 150)
+    text_label = tk.Label( canvas_principal1, text="El número binario es:", font=(t_font,subt_font_size), background="#2A5FB7").place(x = 350, y = 150)
+    bin_label = tk.Label( canvas_principal1, text= binario_ext[2:],font=(t_font,subt_font_size), background="#2A5FB7").place(x = 380, y = 190)
+    text2_label = tk.Label( canvas_principal1, text="Escoge la paridad:", font=(t_font,subt_font_size), background="#2A5FB7").place(x = 360, y = 230)
+    parity = BooleanVar()
+    odd = Radiobutton(canvas_principal1,text="Odd parity", variable=parity, value=False , font=(t_font,subt_font_size),fg='#000000',highlightcolor=btn_color, background="#2A5FB7")
+    odd.place(x=380,y=300)
+    even = Radiobutton(canvas_principal1,text="Even parity", variable=parity, value=True , font=(t_font,subt_font_size),fg='#000000',highlightcolor=btn_color, background="#2A5FB7")
+    even.place(x=380,y=270)
+
+    '''
+This function tries to invoke the Hamming encoding module, and after getting the response, 
+it proceeds to create the encoding table. 
+In case of failure, it displays an alert to the user to handle different error cases.
+'''
+    def encode():
+        try:
+            int(binario_ext[2:],2)
+            encoded = Hammin_code.hamming_encode(binario_ext[2:],parity.get())
+            create_new_encription(encoded)
+        except:
+            if binario_ext[2:] == "":
+                tk.messagebox.showwarning(title="Alert!", message="Por favor introduzca un número binario")
+            else:
+                tk.messagebox.showerror(title="Error!", message="Número binario inválido")
+
+    '''
+    This function tries to make a call to the Hamming encoding check module, 
+    to execute with the response to execute the creation of the encoding check table. 
+    If it fails it displays an alert to the user for different error cases.
+    '''
+    def check():
+        try:
+            int(binario_ext[2:],2)
+            checked = Hammin_code.check_hamming_encode(binario_ext[2:],parity.get())
+            create_new_check(checked[0],checked[1],int(parity.get()))
+        except:
+            if binario_ext[2:] == "":
+                tk.messagebox.showwarning(title="Alert!", message="Por favor introduzca un número binario")
+            elif len(binario_ext[2:]) == 1:
+                tk.messagebox.showerror(title="Error!", message="El número binario deber ser de mayor logintud")
+            else:
+                tk.messagebox.showerror(title="Error!", message="Número binario inválido")
+
+
+
+    '''
+   This function creates and displays a coding table according to the data in list
+
+    @param list: A list of encoding returned by Hammin_code.py
+
+    '''
+    def create_new_encription(list):
+
+        # find total number of rows and
+        # columns in list
+        total_rows = len(list)+1
+        total_columns = len(list[-1])+1
+
+        window_w = (total_columns-1)*37 + 188
+        window_h = total_rows*27
+
+        new_encript = Toplevel()
+        new_encript.resizable(False, False)
+        new_encript.title('Encoded number')
+        new_encript.geometry("%dx%d+0+0" % (window_w, window_h))
+
+        # Canvas
+        encript_canvas = Canvas(new_encript, width= window_w, height=window_h, bg=bg_color)
+        encript_canvas.place(x=-2, y=-2)
+
+        d=0
+        p=0
+        
+        for i in range(total_rows):
+                for j in range(total_columns):
+
+                    #(0,0)
+                    if i==0 and j==0:
+                        Entry(encript_canvas, width=17, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold')).grid(row=i,column=j)
+
+                    #(1,0)
+                    elif i==1 and j==0:
+                        e = Entry(encript_canvas, width=17, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, "Input(No parity)")
+                        p=0
+                        d=0
+
+                    #(p's column)
+                    elif i!=0 and i!=1 and  j==0 and i != total_rows - 1:
+                        e = Entry(encript_canvas, width=17, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i, column=j)
+                        e.insert(END, "p" + str(i-1))
+
+                    #(n,0)
+                    elif i == total_rows-1 and j==0:
+                        e = Entry(encript_canvas, width=17, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, "Output(with parity)")
+
+                    #(p & d row)
+                    elif i==0 and j!=0:
+                        e = Entry(encript_canvas, width=3, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        
+                        e.grid(row=i, column=j)
+                        if j == 2**p:
+                            e.insert(END, "p" + str(p+1))
+                            p=p+1
+                        else:
+                            d=d+1
+                            e.insert(END, "d" + str(d))
+                    
+                    #(input string)
+                    elif i==1 and j!=0:
+                        if j != 2**p:
+                            e = Entry(encript_canvas, width=3, fg=text_color, background=bg_color,
+                                        font=(t_font,subt_font_size,'bold'))
+                            e.grid(row=i, column=j)
+                            e.insert(END,list[0][d])    
+                            d=d+1
+                        else:
+                            e = Entry(encript_canvas, width=3, fg=text_color, background=bg_color,
+                                        font=(t_font,subt_font_size,'bold'))
+                            e.grid(row=i, column=j)
+                            p=p+1
+                        
+                    #(output string)
+                    elif i==total_rows-1:
+                        e = Entry(encript_canvas, width=3, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        
+                        e.grid(row=i, column=j)
+                        e.insert(END,list[-1][j-1])
+                    
+                    #(inner lists)
+                    else:
+                        e = Entry(encript_canvas, width=3, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        
+                        e.grid(row=i, column=j)
+                        e.insert(END,list[i-1][j-1])
+
+    '''
+    This function alerts the user if errors are found in a coding and where.
+    It also creates and displays a coding check table according to the data in list.
+
+    @param list: A list of encodings returned by Hammin_code.py.
+
+    @param error: The bit position where the error is.
+
+    @param parity: A boolean with the parity of the encoding (even = true ; odd = false)
+
+    '''
+    def create_new_check(list,error,parity):
+        if error == 0: 
+            tk.messagebox.showinfo(title="Success!", message="Ningún error encontrado")
+        else:
+            tk.messagebox.showerror(title="Error detected!",message="Error encontrado en el bit #" + str(error))
+
+        # find total number of rows and
+        # columns in list
+        total_rows = len(list)+1
+        total_columns = len(list[0])+3
+
+        window_w = (total_columns-3)*37 + (110*3)
+        window_h = total_rows*27
+
+        new_check = Toplevel()
+        new_check.resizable(False, False)
+        new_check.title('Checked parity')
+        new_check.geometry("%dx%d+0+0" % (window_w, window_h))
+
+        # Canvas
+        check_canvas = Canvas(new_check, width= window_w, height=window_h, bg=bg_color)
+        check_canvas.place(x=-2, y=-2)
+
+        d=0
+        p=0
+        
+        for i in range(total_rows):
+                for j in range(total_columns):
+
+                    #(0,0)
+                    if i==0 and j==0:
+                        Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold')).grid(row=i,column=j)
+                    #(1,0)
+                    elif i==1 and j==0:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, "Input")
+                        p=0
+                        d=0
+
+                    #(0,m-1)
+                    elif i==0 and j==total_columns-1:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, "Parity Bit")
+
+                    #(0,m-2)
+                    elif i==0 and j==total_columns-2:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, "Parity check")
+
+                    #(1,m-1)
+                    elif i==1 and j==total_columns-1:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+
+                    #(2,m-2)
+                    elif i==1 and j==total_columns-2:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i,column=j)
+                        e.insert(END, str(parity))
+
+                    #(p's column)
+                    elif i!=0 and i!=1 and j==0:
+                        e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        e.grid(row=i, column=j)
+                        e.insert(END, "p" + str(i-1))
+
+                    #(p & d row)
+                    elif i==0 and j!=0:
+                        e = Entry(check_canvas, width=3, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        
+                        e.grid(row=i, column=j)
+                        if j == 2**p:
+                            e.insert(END, "p" + str(p+1))
+                            p=p+1
+                        else:
+                            d=d+1
+                            e.insert(END, "d" + str(d))
+                    
+                    elif i==1 and j!=0 and j < total_columns-2:
+                        e = Entry(check_canvas, width=3, fg=text_color, background=bg_color,
+                                    font=(t_font,subt_font_size,'bold'))
+                        
+                        e.grid(row=i, column=j)
+                        e.insert(END,list[0][j-1])
+                        
+                    else:
+
+                        this_list = list[i-1]
+
+                        if j == total_columns-1:
+                            e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                        font=(t_font,subt_font_size,'bold'))
+                            e.grid(row=i, column=j)
+                            e.insert(END,this_list[0])
+
+                        elif j == total_columns-2:
+                            e = Entry(check_canvas, width=10, fg=text_color, background=bg_color,
+                                        font=(t_font,subt_font_size,'bold'))
+                            e.grid(row=i, column=j)
+                            e.insert(END,this_list[1])
+
+                        else:
+                            e = Entry(check_canvas, width=3, fg=text_color, background=bg_color,
+                                        font=(t_font,subt_font_size,'bold'))
+                            e.grid(row=i, column=j)
+                            e.insert(END,this_list[2][j-1])
+    encode_btn = Button(canvas_principal1, text="Codificar", command=encode, font=(t_font, subt_font_size), fg='black', bg='lightblue', activebackground='lightblue', activeforeground='black')
+    encode_btn.config(height=2, width=15, relief='raised', bd=2, highlightthickness=0)
+    encode_btn.place(x=250, y=350)
+
+    encode_btn = Button(canvas_principal1, text="Revisar", command=check, font=(t_font, subt_font_size), fg='black', bg='lightblue', activebackground='lightblue', activeforeground='black')
+    encode_btn.config(height=2, width=15, relief='raised', bd=2, highlightthickness=0)
+    encode_btn.place(x=450, y=350)
+
     canvas_principal1.pack()
 
 def openNewWindow_Conversion():
